18d300d7b3a939531c368b7bdec401fcae4e2f1c
sydneycode <sstaggs12@gmail.com>
2018-09-06 11:43:29 -0700

    integrated jeff's code with the top 4 recent results buttons

--- jeff-logic1.js
+++ jeff-logic1.js
@@ -4,44 +4,91 @@ console.log("js is loaded");
 $(document).ready(function () {
 
     var userSearch = $("#search");
+    
 
     userSearch.on("click", function () {
 
         var search = $("#keyword").val();
-        //console.log(search);
 
+        // User input validation -- ensure that some form of input has been entered prior to running a search
+        if (search != "") {
+            
+            //console.log(search);
+
+            var queryURL = "https://cors.io/?http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + search + "&api_key=8008974b60dc438fc58b3ca8d8e82fae&format=json";
+
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+
+            }).then(function (responseLast) {
+
+                var data = JSON.parse(responseLast);
+                console.log(data);
+
+                console.log(data.artist.bio.content);
+                console.log(data.artist.name);
+
+                $("#outputSummary").text(data.artist.bio.content);
+                $("#outputName").html(data.artist.name);
+
+                var imgURL = data.artist.image[3]["#text"];
+
+                var imageU = $("<img>").attr("src", imgURL);
+
+                var artistDiv = $("<div class='ArtistImage'>");
+
+                console.log(data.artist.image[3]["#text"]);
+
+                artistDiv.append(imageU);
+
+                $("#outputImage").html(artistDiv);
+            });
+        }
+    });
+    // Button listener for dynamic search result buttons
+    $(document).on("click", ".btn-s", function() {
+        console.log("HERE1");
+        var prevSearch = $(this).text();
+        
+        searchForResults(prevSearch);
+    });
+
+    function searchForResults(searchTerm) {
+        var search = searchTerm;
+        console.log(search);
         var queryURL = "https://cors.io/?http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + search + "&api_key=8008974b60dc438fc58b3ca8d8e82fae&format=json";
 
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
+                $.ajax({
+                    url: queryURL,
+                    method: "GET"
 
-        }).then(function (responseLast) {
+                }).then(function (responseLast) {
 
-            var data = JSON.parse(responseLast);
-            console.log(data);
+                    var data = JSON.parse(responseLast);
+                    console.log(data);
 
-            console.log(data.artist.bio.content);
-            console.log(data.artist.name);
+                    console.log(data.artist.bio.content);
+                    console.log(data.artist.name);
 
-            $("#outputSummary").text(data.artist.bio.content);
-            $("#outputName").html(data.artist.name);
+                    $("#outputSummary").text(data.artist.bio.content);
+                    $("#outputName").html(data.artist.name);
 
-            var imgURL = data.artist.image[3]["#text"];
+                    var imgURL = data.artist.image[3]["#text"];
 
-            var imageU = $("<img>").attr("src", imgURL);
+                    var imageU = $("<img>").attr("src", imgURL);
 
-            var artistDiv = $("<div class='ArtistImage'>");
+                    var artistDiv = $("<div class='ArtistImage'>");
 
-            console.log(data.artist.image[3]["#text"]);
+                    console.log(data.artist.image[3]["#text"]);
 
-            artistDiv.append(imageU);
+                    artistDiv.append(imageU);
 
-            $("#outputImage").html(artistDiv);
+                    $("#outputImage").html(artistDiv);
+                });
+    }
+        
+});
 
-        })
-
-         
-    })
-})
--- jeff-logic2.js
+++ jeff-logic2.js
@@ -10,9 +10,50 @@ $(document).ready(function () {
         var search = $("#keyword").val();
         //console.log("click")
 
+        // User input validation -- ensure that some form of input has been entered prior to running a search
+        if (search != "") {
+            var queryURL = "https://cors.io/?https://newsapi.org/v2/everything?q=" + search + "&apiKey=7673a23a71a64738bd9a8c446bd4b0b0";
+
+
+
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+    
+            }).then(function (responseLast) {
+    
+                var data = JSON.parse(responseLast);
+                console.log(data);
+    
+                console.log(data.articles[0].title);
+                console.log(data.articles[0].author);
+                console.log(data.articles[0].description);
+                console.log(data.articles[0].url);
+    
+                $("#articlesTitle").text(data.articles[0].title);
+    
+                $("#articlesAuthor").text(data.articles[0].author);
+    
+                $("#articlesDescription").text(data.articles[0].description);
+    
+                $("#articlesUrl").html("<a href='" + data.articles[0].url + "' target='_blank'>" + data.articles[0].url + "</a>");
+            });
+        }
+
+        $("#keyword").val("");
+    });
+    // Button listener for dynamic search result buttons
+    $(document).on("click", ".btn-s", function() {
+        console.log("HERE2");
+        var prevSearch = $(this).text();
+        console.log(prevSearch);
+        searchForResults(prevSearch);
+    });
+
+
+    function searchForResults(searchTerm) {
+        var search = searchTerm;
         var queryURL = "https://cors.io/?https://newsapi.org/v2/everything?q=" + search + "&apiKey=7673a23a71a64738bd9a8c446bd4b0b0";
-
-
 
         $.ajax({
             url: queryURL,
@@ -35,8 +76,6 @@ $(document).ready(function () {
             $("#articlesDescription").text(data.articles[0].description);
 
             $("#articlesUrl").html("<a href='" + data.articles[0].url + "' target='_blank'>" + data.articles[0].url + "</a>");
-        })
-        $("#keyword").val("");
-    });
-
-})
+        });
+    }
+});

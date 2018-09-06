c2d820d97de800906390c25337ed918e4431dba5
sydneycode <sstaggs12@gmail.com>
2018-09-06 13:55:15 -0700

    cleaned up code in my javascript file

--- project-logic-youtube.js
+++ project-logic-youtube.js
@@ -29,28 +29,23 @@ var NUMBER_OF_ARTISTS_STORED_IN_FIREBASE = 5;
 var NUMBER_OF_VIDEOS_STORED_PER_ARTIST = 3;
 var DEFAULT_ARTIST = "BTS";
 
-
 $("#search").on("click", function () {
 
     $("#recent").html("Most Recent Search Result:");
     $("#save-fave").show();
+
     var artist = $("#keyword").val().trim();
     //console.log(artist);
-
-    
 
     // User input validation -- ensure that some form of input has been entered prior to running a search
     if (artist === "") {
         $("#no-search-term").html("<i>Please enter the name of an artist you'd like to search for.</i>");
-
-        //updateSideBar();
     }
     else {
 
         $(".video").empty();
         $("#no-search-term").empty();
-        // $("#favebtn").append('<a class="waves-effect blue-grey btn" id="save-fave">Save this artist as favorite</a>');
-
+       
         var apikey = "AIzaSyAwFXf47eDtC2euhvpRSvmo3ntTJlaILcA";
         var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet" +
             "&order=viewCount" +
@@ -59,7 +54,7 @@ $("#search").on("click", function () {
             "&maxResults=" + NUMBER_OF_VIDEOS_RETRIEVED_FROM_YOUTUBE +
             "&q=" + artist + "&key=" + apikey;
         
-        var videoList = [];
+        //var videoList = [];
         var vidIDs = [];
        
         //perform an AJAX GET request to access data from the specified URL
@@ -69,7 +64,6 @@ $("#search").on("click", function () {
         })
         .then(function (response) {
             //console.log(JSON.stringify(response));
-            // console.log(response.items[0].snippet.title);
             recentSearchItem.artist = artist;
             recentSearchItem.top5videos = [];
 
@@ -77,8 +71,6 @@ $("#search").on("click", function () {
                 // console.log(response.items[i].snippet.title);
                 // console.log(response.items[i].id.videoId);
                 // console.log(response.items[i].snippet.thumbnails.medium.url);
-                // //console.log(response.items[i].statistics.viewCount);
-                // console.log(response.items[i].snippet.description);
 
                 var title = response.items[i].snippet.title;
                 var titleP = "<p>" + title + "</p>";
@@ -93,22 +85,14 @@ $("#search").on("click", function () {
                 var imageLinkSmall = "<a href='" + link + "' target='_blank'><img src='" + thumbnailSmall + "' ></a>";
                 var imageSmall = "<img src='" + thumbnailSmall + "'>";
                 var titleLink = "<a href='" + link + "' target='_blank'>" + title + "</a>";
-
-                //var viewCountP = getViewCount(vidId);
-                //console.log(viewCountP);
                 
                 $("#video" + i).html(imageLink);
                 $("#video" + i).append(titleP + '<br>');
                 $("#video" + i).append(descriptionP + '<br>');
-                // $("#youtube-thumbnails").append(imageLink);
-                // $("#youtube-thumbnails").append(titleP);
-                // $("#youtube-thumbnails").append(descriptionP);
 
-                //$("#youtube-thumbnails").append(viewCountP);
                 if (i < NUMBER_OF_VIDEOS_STORED_PER_ARTIST) {
                     recentSearchItem.top5videos.push([imageLinkSmall, title]);
                 }
-
             }
 
             updateSideBar();
@@ -138,9 +122,11 @@ $("#search").on("click", function () {
             });
         });
 
+        // now emptying the keyword input element from one of Jeff's scripts, because
+        // my script (Sydney's script) was emptying the keyword before it was getting
+        // passed to Jeff's scripts
         //$("#keyword").val("");
     }
-
 });
 
 
@@ -157,16 +143,7 @@ $("#save-fave").on("click", function () {
         favArtist: favArtistItem,
         recentSearchList: recentSearchList
     });
-    // $("#fav-artist").empty();
-    // if (favArtistItem.artist !== "") {
-    //     $("#fav-artist").append("<p>Favorite Artist:");
-    //     $("#fav-artist").append("<h3>" + favArtistItem.artist + "</h3>");
-    //     for (var j = 0; j < favArtistItem.top5videos.length; j++) {
-    //         $("#fav-artist").append(favArtistItem.top5videos[j]);
-    //     }
-    // }
     updateSideBar();
-
 });
 
 // Button listener for dynamic search result buttons
@@ -183,13 +160,10 @@ $(document).ready(function() {
     searchDefault();
 });
 
-
 // Store a reference to the latest value of the recent search list
 // as retrieved from firebase
 database.ref().on("value", function(snapshot) {
-    console.log(snapshot.val());
-    //$("#click-value").text(snapshot.val().clickCount);
-    //clickCounter = snapshot.val().clickCount;
+    //console.log(snapshot.val());
     
     recentSearchList = snapshot.val().recentSearchList;
     favArtistItem = snapshot.val().favArtist;
@@ -221,7 +195,7 @@ function addViewCount(id, i) {
             .then(function (response2) {
                 //console.log(JSON.stringify(response2));
                 var views = response2.items[0].statistics.viewCount
-                console.log(addCommas(views));
+                //console.log(addCommas(views));
                 var viewCount = addCommas(views);
                 var viewCountP = "<p>Number of Views: " + viewCount + "</p>";
                 $("#video" + i + "-1").html(viewCountP);
@@ -229,9 +203,7 @@ function addViewCount(id, i) {
 }
 
 function updateSideBar() {
-    //$("#fav-artist").empty();
-    //$("#search-history").empty();
-
+    
     if (favArtistItem.artist !== "") {
         $("#fa" + 0).html(favArtistItem.artist.toUpperCase());
         for (var j = 0; j < favArtistItem.top5videos.length-2; j++) {
@@ -242,13 +214,14 @@ function updateSideBar() {
         }
     }
 
-    if (recentSearchList.length > 0) {
+    // Checking to see if there's anything stored in the recent seach list before displaying it
+    //if (recentSearchList.length > 0) {
         // $("#search-history").append("<p>5 Most Recent Searches:");
         // $("#search-history-1").append('<div class="card blue-grey darken-1">' +
         //     '<div class="card-content white-text">'+
         //     '<p>Most Recent Searches</p></div></div>');
-       
-    }
+    //}
+
     for (var i = 0; i < recentSearchList.length; i++) {
 
        $("#a" + i).html(recentSearchList[i].artist.toUpperCase());
@@ -277,17 +250,12 @@ function searchForResults(searchTerm) {
     $("#save-fave").show();
 
     var artist = searchTerm;
-    //console.log(artist);
-
     $(".video").empty();
     $("#no-search-term").empty();
-    // $("#favebtn").append('<a class="waves-effect blue-grey btn" id="save-fave">Save this artist as favorite</a>');
 
     // User input validation -- ensure that some form of input has been entered prior to running a search
     if (artist === "") {
         $("#no-search-term").html("<i>Please enter the name of an artist you'd like to search for.</i>");
-
-        updateSideBar();
     }
     else {
         var apikey = "AIzaSyAwFXf47eDtC2euhvpRSvmo3ntTJlaILcA";
@@ -298,7 +266,7 @@ function searchForResults(searchTerm) {
             "&maxResults=" + NUMBER_OF_VIDEOS_RETRIEVED_FROM_YOUTUBE +
             "&q=" + artist + "&key=" + apikey;
         
-        var videoList = [];
+        //var videoList = [];
         var vidIDs = [];
        
         //perform an AJAX GET request to access data from the specified URL
@@ -308,7 +276,6 @@ function searchForResults(searchTerm) {
         })
         .then(function (response) {
             //console.log(JSON.stringify(response));
-            // console.log(response.items[0].snippet.title);
             recentSearchItem.artist = artist;
             recentSearchItem.top5videos = [];
 
@@ -335,9 +302,7 @@ function searchForResults(searchTerm) {
                 if (i < NUMBER_OF_VIDEOS_STORED_PER_ARTIST) {
                     recentSearchItem.top5videos.push([imageLinkSmall, title]);
                 }
-
             }
-
             updateSideBar();
 
             // If there are fewer than 5 recent search results, then add the most recent result 
@@ -364,101 +329,82 @@ function searchForResults(searchTerm) {
                 recentSearchList: recentSearchList
             });
         });
-
-        //$("#keyword").val("");
     }
 }
 
 function searchDefault() {
     var artist = DEFAULT_ARTIST;
-    //console.log(artist);
+    
+    var apikey = "AIzaSyAwFXf47eDtC2euhvpRSvmo3ntTJlaILcA";
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet" +
+        "&order=viewCount" +
+        "&type=video" +
+        "&videoEmbeddable=true" +
+        "&maxResults=" + NUMBER_OF_VIDEOS_RETRIEVED_FROM_YOUTUBE +
+        "&q=" + artist + "&key=" + apikey;
+    
+    //var videoList = [];
+    var vidIDs = [];
+    
+    //perform an AJAX GET request to access data from the specified URL
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function (response) {
+        //console.log(JSON.stringify(response));
+        recentSearchItem.artist = artist;
+        recentSearchItem.top5videos = [];
 
-    // $(".video").empty();
-    // $("#no-search-term").empty();
-    // $("#favebtn").append('<a class="waves-effect blue-grey btn" id="save-fave">Save this artist as favorite</a>');
-
-    // User input validation -- ensure that some form of input has been entered prior to running a search
-    // if (artist === "") {
-    //     $("#no-search-term").html("<i>Please enter the name of an artist you'd like to search for.</i>");
-
-    //     updateSideBar();
-    // }
-    // else {
-        var apikey = "AIzaSyAwFXf47eDtC2euhvpRSvmo3ntTJlaILcA";
-        var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet" +
-            "&order=viewCount" +
-            "&type=video" +
-            "&videoEmbeddable=true" +
-            "&maxResults=" + NUMBER_OF_VIDEOS_RETRIEVED_FROM_YOUTUBE +
-            "&q=" + artist + "&key=" + apikey;
-        
-        var videoList = [];
-        var vidIDs = [];
-       
-        //perform an AJAX GET request to access data from the specified URL
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) {
-            //console.log(JSON.stringify(response));
-            // console.log(response.items[0].snippet.title);
-            recentSearchItem.artist = artist;
-            recentSearchItem.top5videos = [];
-
-            for (var i = 0; i < response.items.length; i++) {
-
-                var title = response.items[i].snippet.title;
-                var titleP = "<p>" + title + "</p>";
-                var description = response.items[i].snippet.description;
-                var descriptionP = "<p>" + description + "</p>";
-                var thumbnail = response.items[i].snippet.thumbnails.medium.url;
-                var thumbnailSmall = response.items[i].snippet.thumbnails.default.url;
-                var vidId = response.items[i].id.videoId;
-                vidIDs.push(vidId);
-                var link = "https://www.youtube.com/watch?v=" + vidId;
-                var imageLink = "<a href='" + link + "' target='_blank'><img src='" + thumbnail + "' ></a>";
-                var imageLinkSmall = "<a href='" + link + "' target='_blank'><img src='" + thumbnailSmall + "' ></a>";
-                var imageSmall = "<img src='" + thumbnailSmall + "'>";
-                var titleLink = "<a href='" + link + "' target='_blank'>" + title + "</a>";
-                
-                $("#video" + i).html(imageLink);
-                $("#video" + i).append(titleP + '<br>');
-                $("#video" + i).append(descriptionP + '<br>');
-     
-                if (i < NUMBER_OF_VIDEOS_STORED_PER_ARTIST) {
-                    recentSearchItem.top5videos.push([imageLinkSmall, title]);
-                }
-
-            }
-
-            updateSideBar();
-
-            // If there are fewer than 5 recent search results, then add the most recent result 
-            // to the front of the list
-            // Otherwise, remove the last result in the list, and then add the most recent result
-            // to the front of the list
-            if (recentSearchList.length < NUMBER_OF_ARTISTS_STORED_IN_FIREBASE) {
-                recentSearchList.unshift(recentSearchItem);
-            }
-            else {
-                console.log(recentSearchList);
-                recentSearchList.splice(NUMBER_OF_ARTISTS_STORED_IN_FIREBASE - 1, 1);
-                console.log(recentSearchList);
-                recentSearchList.unshift(recentSearchItem);
-            }
-        
-            for (var i = 0; i < NUMBER_OF_VIDEOS_RETRIEVED_FROM_YOUTUBE; i++) {
-                var vi = vidIDs[i];
-                addViewCount(vi, i);
-            }
+        for (var i = 0; i < response.items.length; i++) {
+            var title = response.items[i].snippet.title;
+            var titleP = "<p>" + title + "</p>";
+            var description = response.items[i].snippet.description;
+            var descriptionP = "<p>" + description + "</p>";
+            var thumbnail = response.items[i].snippet.thumbnails.medium.url;
+            var thumbnailSmall = response.items[i].snippet.thumbnails.default.url;
+            var vidId = response.items[i].id.videoId;
+            vidIDs.push(vidId);
+            var link = "https://www.youtube.com/watch?v=" + vidId;
+            var imageLink = "<a href='" + link + "' target='_blank'><img src='" + thumbnail + "' ></a>";
+            var imageLinkSmall = "<a href='" + link + "' target='_blank'><img src='" + thumbnailSmall + "' ></a>";
+            var imageSmall = "<img src='" + thumbnailSmall + "'>";
+            var titleLink = "<a href='" + link + "' target='_blank'>" + title + "</a>";
             
-            // database.ref().set({
-            //     favArtist: favArtistItem,
-            //     recentSearchList: recentSearchList
-            // });
-        });
+            $("#video" + i).html(imageLink);
+            $("#video" + i).append(titleP + '<br>');
+            $("#video" + i).append(descriptionP + '<br>');
+    
+            if (i < NUMBER_OF_VIDEOS_STORED_PER_ARTIST) {
+                recentSearchItem.top5videos.push([imageLinkSmall, title]);
+            }
+        }
 
-        //$("#keyword").val("");
-    //}
+        updateSideBar();
+
+        // If there are fewer than 5 recent search results, then add the most recent result 
+        // to the front of the list
+        // Otherwise, remove the last result in the list, and then add the most recent result
+        // to the front of the list
+        if (recentSearchList.length < NUMBER_OF_ARTISTS_STORED_IN_FIREBASE) {
+            recentSearchList.unshift(recentSearchItem);
+        }
+        else {
+            console.log(recentSearchList);
+            recentSearchList.splice(NUMBER_OF_ARTISTS_STORED_IN_FIREBASE - 1, 1);
+            console.log(recentSearchList);
+            recentSearchList.unshift(recentSearchItem);
+        }
+    
+        for (var i = 0; i < NUMBER_OF_VIDEOS_RETRIEVED_FROM_YOUTUBE; i++) {
+            var vi = vidIDs[i];
+            addViewCount(vi, i);
+        }
+        
+        // We're not storing the search results for the "default artist" in firebase
+        // database.ref().set({
+        //     favArtist: favArtistItem,
+        //     recentSearchList: recentSearchList
+        // });
+    });
 }
